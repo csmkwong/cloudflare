9a1fb25283efa7767cccf7a448d9269c483c4b6e
--add <--add>
2022-10-12 22:18:23 +0100

    updated the file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
+import BasicGrid from './BasicGrid';
+
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return
+  {
+    <BasicGrid></BasicGrid>
+  }
+
+
 }
 
 export default App;
+
+
