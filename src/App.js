fc6f3ea99bbef10b73532b23e1f251ffa3b34d8b
--add <--add>
2022-10-12 22:28:55 +0100

    updated the file

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid'; // Grid version 1
 import Grid2 from '@mui/material/Unstable_Grid2'; // Grid version 2
 import BasicGrid from './BasicGrid';
+import { DataGrid } from '@mui/x-data-grid';
 
 
 function App() {
   return (
 
     <BasicGrid></BasicGrid>
+    <Box sx={{ height: 400, width: '100%' }}>
+    <DataGrid
+      rows={rows}
+      columns={columns}
+      pageSize={5}
+      rowsPerPageOptions={[5]}
+      checkboxSelection
+      disableSelectionOnClick
+      experimentalFeatures={{ newEditingApi: true }}
+    />
+  </Box>
   )
    
 
