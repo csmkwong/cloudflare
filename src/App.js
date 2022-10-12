c2c9d9b34eebeb379c736d11d2f83bae4fa267a7
--add <--add>
2022-10-12 21:53:30 +0100

    updated the file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
-    <div className="App">
-    <p>Contact Me</p>
-    <div>
-    <form action="/action_page.php">
-    <label>First Name</label>
-    <input type="text" id="fname" name="firstname" placeholder="Your name.." />
-    <label>Last Name</label>
-    <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
-
-
-    <label>Email</label>
-    <input type="email" id="email" name="email" placeholder="Your email" />
-
-
-    <label>Subject</label>
-    <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
-    <input type="submit" value="Submit" />
-    </form>
-    </div>
-    </div>
- 
-  );
+  return <Button variant="contained">Hello World</Button>;
 }
-
-export default App;
