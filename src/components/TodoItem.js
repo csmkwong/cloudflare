a11f81548a0b0b8e27dcc05db1a8dba567bd0dd3
--add <--add>
2022-10-13 18:54:51 +0100

    updated the file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,7 +3,10 @@ import React from "react"
 function TodoItem(props) {
     return (
         <li>
-            <input type="checkbox" checked={props.todo.completed} /> {props.todo.title}
+            <input type="checkbox" 
+            checked={props.todo.completed} 
+            onChange={() => console.log("clicked")}
+            /> {props.todo.title}
         </li>
     )
 
